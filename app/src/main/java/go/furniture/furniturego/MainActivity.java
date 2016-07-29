6ece05546eb5db2aec2420f2fdcbdbb553cfe132
adamliu84 <adam.liu84@gmail.com>
2016-07-30 01:25:36 +0800

    Save image codebase

--- app/src/main/java/go/furniture/furniturego/MainActivity.java
+++ app/src/main/java/go/furniture/furniturego/MainActivity.java
@@ -1,5 +1,7 @@
 package go.furniture.furniturego;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -12,17 +14,71 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private final String NameOfFolder = "/FurnitureGo";
+    private final String NameOfFile = "furGogo";
+    private ImageView mImageView;
+    private Bitmap mBitmap;
+
+    public static Bitmap decodeSampledBitmapFromFile(String path, int reqWidth, int reqHeight) { // BEST QUALITY MATCH
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path, options);
+        // Calculate inSampleSize
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        options.inPreferredConfig = Bitmap.Config.RGB_565;
+        int inSampleSize = 1;
+        if (height > reqHeight) {
+            inSampleSize = Math.round((float) height / (float) reqHeight);
+        }
+        int expectedWidth = width / inSampleSize;
+        if (expectedWidth > reqWidth) {
+            //if(Math.round((float)width / (float)reqWidth) > inSampleSize) // If bigger SampSize..
+            inSampleSize = Math.round((float) width / (float) reqWidth);
+        }
+        options.inSampleSize = inSampleSize;
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(path, options);
+    }
+
+    public static Bitmap addText(Bitmap srcBitmap, String gText) {
+        Bitmap destBitmap = srcBitmap.copy(srcBitmap.getConfig(), true);
+        Canvas canvas = new Canvas(destBitmap);
+        // new antialised Paint
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        // text color - #3D3D3D
+        paint.setColor(Color.rgb(61, 61, 61));
+        // text size in pixels
+        paint.setTextSize((int) (20 * 1.5));
+        // text shadow
+        paint.setShadowLayer(1f, 0f, 1f, Color.WHITE);
+        // draw text to the Canvas center
+        Rect bounds = new Rect();
+        paint.getTextBounds(gText, 0, gText.length(), bounds);
+        int x = (destBitmap.getWidth() - bounds.width()) / 2;
+        int y = (destBitmap.getHeight() + bounds.height()) / 2;
+        canvas.drawText(gText, x, y, paint);
+        return destBitmap;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-              dispatchTakePictureIntent();
+                dispatchTakePictureIntent();
             }
         });
     }
@@ -62,72 +118,77 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-            File file = new File(Environment.getExternalStorageDirectory()+ File.separator + "image.jpg");
+            File file = new File(Environment.getExternalStorageDirectory() + File.separator + "image.jpg");
             intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
             startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
         }
     }
 
-    private ImageView mImageView;
+    public void onFurPreviewSave(View view) {
+        saveImage(this.getBaseContext(), addText(mBitmap, getCurrentDateAndTime() + "Test"));
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            File file = new File(Environment.getExternalStorageDirectory()+File.separator + "image.jpg");
+            File file = new File(Environment.getExternalStorageDirectory() + File.separator + "image.jpg");
             mImageView = (ImageView) findViewById(R.id.imvFurPreview);
-            //mImageView.setImageBitmap(decodeSampledBitmapFromFile(file.getAbsolutePath(), 500, 250));
-            mImageView.setImageBitmap(addText(decodeSampledBitmapFromFile(file.getAbsolutePath(), 500, 250)));
+            mBitmap = decodeSampledBitmapFromFile(file.getAbsolutePath(), 800, 600);
+            mImageView.setImageBitmap(mBitmap);
         }
     }
 
-    public static Bitmap decodeSampledBitmapFromFile(String path, int reqWidth, int reqHeight) { // BEST QUALITY MATCH
-        // First decode with inJustDecodeBounds=true to check dimensions
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(path, options);
-        // Calculate inSampleSize
-        // Raw height and width of image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        options.inPreferredConfig = Bitmap.Config.RGB_565;
-        int inSampleSize = 1;
-        if (height > reqHeight) {
-            inSampleSize = Math.round((float)height / (float)reqHeight);
+    public void saveImage(Context context, Bitmap ImageToSave) {
+        // Create imageDir
+        String file_path = Environment.getExternalStorageDirectory().getAbsolutePath() + NameOfFolder;
+        String CurrentDateAndTime = getCurrentDateAndTime();
+        File directory = new File(file_path);
+        if (!directory.exists()) {
+            directory.mkdirs();
         }
-        int expectedWidth = width / inSampleSize;
-        if (expectedWidth > reqWidth) {
-            //if(Math.round((float)width / (float)reqWidth) > inSampleSize) // If bigger SampSize..
-            inSampleSize = Math.round((float)width / (float)reqWidth);
+        // Create image
+        File imageFile = new File(directory, NameOfFile + CurrentDateAndTime + ".jpg");
+        Log.d("imageFile", imageFile.getAbsolutePath());
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(imageFile);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            ImageToSave.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+                addImageGallery(imageFile);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         }
-        options.inSampleSize = inSampleSize;
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false;
-        return BitmapFactory.decodeFile(path, options);
     }
 
-    public static Bitmap addText(Bitmap srcBitmap){
-        String gText = "test";
-        Bitmap destBitmap = srcBitmap.copy(srcBitmap.getConfig(), true);
-        Canvas canvas = new Canvas(destBitmap);
-        // new antialised Paint
-        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        // text color - #3D3D3D
-        paint.setColor(Color.rgb(61, 61, 61));
-        // text size in pixels
-        paint.setTextSize((int) (20 * 1.5));
-        // text shadow
-        paint.setShadowLayer(1f, 0f, 1f, Color.WHITE);
-        // draw text to the Canvas center
-        Rect bounds = new Rect();
-        paint.getTextBounds(gText, 0, gText.length(), bounds);
-        int x = (destBitmap.getWidth() - bounds.width())/2;
-        int y = (destBitmap.getHeight() + bounds.height())/2;
-        canvas.drawText(gText, x, y, paint);
-        return destBitmap;
+    private String getCurrentDateAndTime() {
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+        String formattedDate = df.format(c.getTime());
+        return formattedDate;
     }
+
+    /**
+     * Register taken photo to gallery
+     * http://stackoverflow.com/questions/9360091/how-to-register-an-image-so-that-it-is-immediately-viewable-from-gallery-applica
+     *
+     * @param file
+     */
+    private void addImageGallery(File file) {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.DATA, file.getAbsolutePath());
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+    }
+
 }
