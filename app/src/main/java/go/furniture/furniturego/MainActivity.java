d0e4c6a73a8ba596c1018e58b173c31d6fe52f9e
adamliu84 <adam.liu84@gmail.com>
2016-08-01 22:55:41 +0800

    Shifted all image manipulation to image manager class

--- app/src/main/java/go/furniture/furniturego/MainActivity.java
+++ app/src/main/java/go/furniture/furniturego/MainActivity.java
@@ -29,6 +29,8 @@ import java.io.FileOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+import go.furniture.furniturego.manager.ImageManager;
+
 public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
@@ -37,52 +39,7 @@ public class MainActivity extends AppCompatActivity {
     Toast mToast;
     private ImageView mImageView;
     private Bitmap mBitmap;
-
-    public static Bitmap decodeSampledBitmapFromFile(String path, int reqWidth, int reqHeight) { // BEST QUALITY MATCH
-        // First decode with inJustDecodeBounds=true to check dimensions
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(path, options);
-        // Calculate inSampleSize
-        // Raw height and width of image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        options.inPreferredConfig = Bitmap.Config.RGB_565;
-        int inSampleSize = 1;
-        if (height > reqHeight) {
-            inSampleSize = Math.round((float) height / (float) reqHeight);
-        }
-        int expectedWidth = width / inSampleSize;
-        if (expectedWidth > reqWidth) {
-            //if(Math.round((float)width / (float)reqWidth) > inSampleSize) // If bigger SampSize..
-            inSampleSize = Math.round((float) width / (float) reqWidth);
-        }
-        options.inSampleSize = inSampleSize;
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false;
-        return BitmapFactory.decodeFile(path, options);
-    }
-
-    public static Bitmap addText(Bitmap srcBitmap, String gText) {
-        Bitmap destBitmap = srcBitmap.copy(srcBitmap.getConfig(), true);
-        Canvas canvas = new Canvas(destBitmap);
-        // new antialised Paint
-        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        // text color - #3D3D3D
-        paint.setColor(Color.rgb(61, 61, 61));
-        // text size in pixels
-        paint.setTextSize((int) (20 * 1.5));
-        // text shadow
-        paint.setShadowLayer(1f, 0f, 1f, Color.WHITE);
-        // draw text to the Canvas center
-        Rect bounds = new Rect();
-        paint.getTextBounds(gText, 0, gText.length(), bounds);
-        int x = (destBitmap.getWidth() - bounds.width()) / 2;
-        int y = (destBitmap.getHeight() + bounds.height()) / 2;
-        canvas.drawText(gText, x, y, paint);
-        return destBitmap;
-    }
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -138,7 +95,7 @@ public class MainActivity extends AppCompatActivity {
         }
         //Get text and save image
         EditText edtFur = (EditText) findViewById(R.id.edtFurPreview);
-        saveImage(this.getBaseContext(), addText(mBitmap, edtFur.getText().toString()));
+        saveImage(this.getBaseContext(), ImageManager.addText(mBitmap, edtFur.getText().toString()));
     }
 
     @Override
@@ -146,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             File file = new File(Environment.getExternalStorageDirectory() + File.separator + "image.jpg");
             mImageView = (ImageView) findViewById(R.id.imvFurPreview);
-            mBitmap = decodeSampledBitmapFromFile(file.getAbsolutePath(), 800, 600);
+            mBitmap = ImageManager.decodeSampledBitmapFromFile(file.getAbsolutePath(), 800, 600);
             mImageView.setImageBitmap(mBitmap);
         }
     }
---
+++ app/src/main/java/go/furniture/furniturego/manager/ImageManager.java
@@ -0,0 +1,60 @@
+package go.furniture.furniturego.manager;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
+/**
+ * Created by adam on 1/8/16.
+ */
+public class ImageManager {
+
+    public static Bitmap decodeSampledBitmapFromFile(String path, int reqWidth, int reqHeight) { // BEST QUALITY MATCH
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path, options);
+        // Calculate inSampleSize
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        options.inPreferredConfig = Bitmap.Config.RGB_565;
+        int inSampleSize = 1;
+        if (height > reqHeight) {
+            inSampleSize = Math.round((float) height / (float) reqHeight);
+        }
+        int expectedWidth = width / inSampleSize;
+        if (expectedWidth > reqWidth) {
+            //if(Math.round((float)width / (float)reqWidth) > inSampleSize) // If bigger SampSize..
+            inSampleSize = Math.round((float) width / (float) reqWidth);
+        }
+        options.inSampleSize = inSampleSize;
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(path, options);
+    }
+
+    public static Bitmap addText(Bitmap srcBitmap, String gText) {
+        Bitmap destBitmap = srcBitmap.copy(srcBitmap.getConfig(), true);
+        Canvas canvas = new Canvas(destBitmap);
+        // new antialised Paint
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        // text color - #3D3D3D
+        paint.setColor(Color.rgb(61, 61, 61));
+        // text size in pixels
+        paint.setTextSize((int) (20 * 1.5));
+        // text shadow
+        paint.setShadowLayer(1f, 0f, 1f, Color.WHITE);
+        // draw text to the Canvas center
+        Rect bounds = new Rect();
+        paint.getTextBounds(gText, 0, gText.length(), bounds);
+        int x = (destBitmap.getWidth() - bounds.width()) / 2;
+        int y = (destBitmap.getHeight() + bounds.height()) / 2;
+        canvas.drawText(gText, x, y, paint);
+        return destBitmap;
+    }
+
+}
